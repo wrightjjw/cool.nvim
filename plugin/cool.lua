f973fa2f902b884330f4bd8d2ff7dfab2b01f6cd
Josh Wright <wright.jjw@gmail.com>
2023-07-26 13:14:25 -0400

    correct the command call

--- plugin/cool.lua
+++ plugin/cool.lua
@@ -2,4 +2,4 @@ vim.api.nvim_create_user_command("CoolNolineyank", function(args)
     local start = args["line1"]
     local fin = args["line2"]
     require('cool').nolineyank(start, fin)
-end)
+end, {range = true})
